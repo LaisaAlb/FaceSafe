9c5edf0f6fb1b88968a9bda67dcb745f2fb0320d
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2024-05-23 09:31:41 -0300

    Adicionando inputs
    Foi adicionado informações como: RG e TEL no momento de inserir os dados

--- js/dados.js
+++ js/dados.js
@@ -1,19 +1,23 @@
-// Atualizado para usar o ID correto
 const continuarButton = document.getElementById("continuarButton");
+
+// Desabilita o botão de continuar se algum campo estiver vazio ao carregar a página
+verificarCampos();
 
 // Adiciona um listener para o evento de clique no botão
 continuarButton.addEventListener("click", function() {
-  // Redireciona para a página desejada
-  window.location.href = "/projeto/face/index.html";
+    // Redireciona para a página desejada
+    window.location.href = "/projeto/face/index.html";
 });
 
 function verificarCampos() {
     var nome = document.getElementById("nome").value;
     var email = document.getElementById("email").value;
     var cpf = document.getElementById("cpf").value;
+    var rg = document.getElementById("rg").value;
+    var tel = document.getElementById("tel").value;
     var codigo = document.getElementById("codigo").value;
 
-    if (nome.trim() !== "" && email.trim() !== "" && cpf.trim() !== "" && codigo.trim() !== "") {
+    if (nome.trim() !== "" && email.trim() !== "" && cpf.trim() !== "" && rg !== "" && tel !== "" && codigo.trim() !== "") {
         continuarButton.disabled = false;
     } else {
         continuarButton.disabled = true;
@@ -24,4 +28,6 @@ function verificarCampos() {
 document.getElementById("nome").addEventListener("input", verificarCampos);
 document.getElementById("email").addEventListener("input", verificarCampos);
 document.getElementById("cpf").addEventListener("input", verificarCampos);
+document.getElementById("rg").addEventListener("input", verificarCampos);
+document.getElementById("tel").addEventListener("input", verificarCampos);
 document.getElementById("codigo").addEventListener("input", verificarCampos);
